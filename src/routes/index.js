4630cc6d219c460fe2f44e7f5c794ac566f9837a
JulianF <juliandavidbf@gmail.com>
2022-06-06 23:26:01 -0500

    @feat: update hash

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import Header from '../templates/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes'
 
 
 //Establecer rutas
@@ -17,6 +19,12 @@ const router = async  () => {
     const header = null || document.getElementById('header');
     const content = null || document.getElementById('content');
     header.innerHTML = await Header();
+
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+
+    content.innerHTML = await render;
 };
 
 export default router;
--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,6 +1,6 @@
 //Obtener la ruta dond esta el usuario
 const getHash = () => 
     //format texting
-    location.hash.slice(1).toLowerCase().split('/')[1] || '/';
+    location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
 
 export default getHash;
