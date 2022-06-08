d3b70f715e0de6e340c34dabd6bac072c89add03
JulianF <juliandavidbf@gmail.com>
2022-06-07 22:11:59 -0500

    @feat:styles update

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
         <div class="Header-logo">
             <h1>
                 <a href="/">
-                100tifi.co
+                    Rickd and Morty
                 </a>
             </h1>
         </div>
